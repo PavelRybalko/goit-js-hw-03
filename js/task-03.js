7ddeb2e3ad82743b32985c0c4cb2a91623481536
Pavel Rybalko <freedom277f@gmail.com>
2020-07-29 17:08:20 +0300

    fix 3,4 task

--- js/task-03.js
+++ js/task-03.js
@@ -1,15 +1,17 @@
-const findBestEmployee = function (employees) {
-  let bestEmployeeScore = 0;
-  let bestEmployeeName;
-  for (const key in employees) {
-    if (employees[key] > bestEmployeeScore) {
-      bestEmployeeScore = employees[key];
-      bestEmployeeName = key;
-    }
-  }
-  return bestEmployeeName;
+const findBestEmployee = function(employees){
+
+let bestEmployeeScore = 0;
+let bestEmployeeName;
+for(let employee of Object.entries(employees)){
+	const [name,score] = employee;
+	if(bestEmployeeScore<score){
+		bestEmployeeScore = score;
+		bestEmployeeName = name; 
+	}
+}
+return bestEmployeeName;
 };
 
-console.log(findBestEmployee({ ann: 29, david: 35, helen: 1, lorence: 99 }));
-console.log(findBestEmployee({ poly: 12, mango: 17, ajax: 4 }));
-console.log(findBestEmployee({ lux: 147, david: 21, kiwi: 19, chelsy: 38 }));
+console.log(findBestEmployee({ann: 29, david: 35, helen: 1, lorence: 99,}));
+console.log(findBestEmployee({poly: 12, mango:17, ajax: 4,}));
+console.log(findBestEmployee({lux: 147, david:21, kiwi: 19, chelsy: 38,}));
--- js/task-04.js
+++ js/task-04.js
@@ -1,11 +1,11 @@
-const countTotalSalary = function (employees) {
-  let total = 0;
-  for (const key in employees) {
-    total += employees[key];
-  }
-  return total;
+const countTotalSalary = function(employees){
+let total = 0;
+for (const value of Object.values(employees)){
+	total += value;
+}
+return total;
 };
 
 console.log(countTotalSalary({}));
-console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
-console.log(countTotalSalary({ kiwi: 200, lux: 50, chelsy: 150 }));
+console.log(countTotalSalary({mango: 100, poly: 150, alfred: 80,}));
+console.log(countTotalSalary({kiwi: 200, lux: 50, chelsy: 150,}));
