8c8f1bdb3760166e71861fcd9acd3ba1346099f3
Pavel Rybalko <freedom277f@gmail.com>
2020-07-29 21:45:04 +0300

    fix destructurization in cycle for

--- js/task-03.js
+++ js/task-03.js
@@ -1,17 +1,15 @@
-const findBestEmployee = function(employees){
-
-let bestEmployeeScore = 0;
-let bestEmployeeName;
-for(let employee of Object.entries(employees)){
-	const [name,score] = employee;
-	if(bestEmployeeScore<score){
-		bestEmployeeScore = score;
-		bestEmployeeName = name; 
-	}
-}
-return bestEmployeeName;
+const findBestEmployee = function (employees) {
+  let bestEmployeeScore = 0;
+  let bestEmployeeName;
+  for (const [name, score] of Object.entries(employees)) {
+    if (bestEmployeeScore < score) {
+      bestEmployeeScore = score;
+      bestEmployeeName = name;
+    }
+  }
+  return bestEmployeeName;
 };
 
-console.log(findBestEmployee({ann: 29, david: 35, helen: 1, lorence: 99,}));
-console.log(findBestEmployee({poly: 12, mango:17, ajax: 4,}));
-console.log(findBestEmployee({lux: 147, david:21, kiwi: 19, chelsy: 38,}));
+console.log(findBestEmployee({ ann: 29, david: 35, helen: 1, lorence: 99 }));
+console.log(findBestEmployee({ poly: 12, mango: 17, ajax: 4 }));
+console.log(findBestEmployee({ lux: 147, david: 21, kiwi: 19, chelsy: 38 }));
